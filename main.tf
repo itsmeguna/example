7f4449d0cdaee4beb1c39b99f3d801338f5f7bf2
itsmeguna <129356942+itsmeguna@users.noreply.github.com>
2023-04-12 16:40:31 +0530

    ec2_module

--- main.tf
+++ main.tf
@@ -1,17 +1,15 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.62.0"
-    }
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "single-instance"
+
+  ami                    = "ami-0aa2b7722dc1b5612"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
   }
-}
-
-provider "aws"{
-    region= "us-east-1"
-
-}
-resource "aws_instance" "public_instance" {
-    ami                     = "ami-0aa2b7722dc1b5612"
-    instance_type           = "t2.micro"
 }
