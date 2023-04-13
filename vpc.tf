03e0f539d24c2eac764a309f048e4c5dee2e837c
itsmegunam@gmail.com <jenkins@ip-172-31-95-28.ec2.internal>
2023-04-13 09:16:25 +0000

    modified_VPC-3subnet

--- vpc.tf
+++ vpc.tf
@@ -1,17 +1,14 @@
 terraform {
-  backend "s3" {
-    bucket = "compliance-devops-terraform-backend"
-    key    = "state/us-west-2"
-    region = "us-west-2"
+  
   }
 
-  required_providers {
+required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
-}
+
 provider "aws" {
   region = "us-west-2"
 }
