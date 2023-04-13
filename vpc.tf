304a30087844e0422329e0ecbc9d24a9b9d252e8
Jenkins <jenkins@ip-172-31-95-28.ec2.internal>
2023-04-13 08:59:45 +0000

    VPC-3subnet

---
+++ vpc.tf
@@ -0,0 +1,38 @@
+terraform {
+  backend "s3" {
+    bucket = "compliance-devops-terraform-backend"
+    key    = "state/us-west-2"
+    region = "us-west-2"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-west-2"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "t4-compliance-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+
+  tags = {
+    Terraform   = "true"
+    Environment = "prod"
+  }
+}
+
