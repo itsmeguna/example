8bc8c03a878337b03f69eafe60d039df20fcc871
itsmeguna <itsmegunam@gmail.com>
2023-04-13 10:26:59 +0000

    modified_vpc

--- vpc.tf
+++ vpc.tf
@@ -1,25 +1,22 @@
 terraform {
-  
-  }
-
 required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
-  }
-
+ }
+}
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "t4-compliance-vpc"
+  name = "my--vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  azs             = ["us-east-1a", "us-east-1a", "us-east-1a"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   
